0f481ce2120a940c84459d7e665373939ec5b602
Hridik Dev <hridikori@gmail.com>
2025-08-16 12:30:10 +0530

    Second Commit

--- .vscode/P11_Seat Number Assignment in a Theater.cpp
+++ .vscode/P11_Seat Number Assignment in a Theater.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main(){
-    int n,i,j,s=0;
+    int n,i,j,count=0;
     cin>>n;
     for(i=0;i<n;i++){
         for(j=0;j<=i;j++){
-            s=j+1;
-            cout<<s<<" ";
+            count++;
+            cout<<count<<" ";
         }
         cout<<endl;
     }
--- .vscode/P12_Stage Decoration Light Setup.cpp
+++ .vscode/P12_Stage Decoration Light Setup.cpp
@@ -4,14 +4,20 @@
 using namespace std;
 
 int main(){
-    int n,i,j,s=0;
+    int n,i,j;
     cin>>n;
     for(i=0;i<n;i++){
+        for(j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
         for(j=0;j<=i;j++){
-            s=j+1;
-            cout<<s<<" ";
+            cout<<"*";
+            if(j!=i){
+                cout<<"*";
+            }
         }
         cout<<endl;
     }
+    
     return 0;
 }
